4c996815302c606bfaf03d41e4561fd335a1b976
jparrafitch <57338495+jparrafitch@users.noreply.github.com>
2019-12-05 17:23:26 -0800

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,11 @@
-console.log("Hi! Welcome to my portfolio!")
+/* exported myFunction */
+console.log('Hi! Welcome to my portfolio!')
 
-function menuToggle() {
-  var x = document.getElementById('myNavtoggle');
+function menuToggle () {
+  var x = document.getElementById('myNavtoggle')
   if (x.className === 'navtoggle') {
-    x.className += ' responsive';
+    x.className += ' responsive'
   } else {
-    x.className = 'navtoggle';
+    x.className = 'navtoggle'
   }
 }
